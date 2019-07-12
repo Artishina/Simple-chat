240c225ed0ba332b2613ea32ccea0faaafb7327b
Artishina <artive112@yandex.ru>
2019-07-12 22:31:24 +0300

    added server part

--- server/src/com/company/chat/server/ChatServer.java
+++ server/src/com/company/chat/server/ChatServer.java
@@ -1,4 +1,66 @@
 package com.company.chat.server;
 
-public class ChatServer {
+import com.company.network.TCPConnection;
+import com.company.network.TCPConnectionListener;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer implements TCPConnectionListener {
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+
+    private final List<TCPConnection> connections = new ArrayList<>();
+
+    private ChatServer() {
+        System.out.println("Server running...");
+        try (ServerSocket serverSocket = new ServerSocket(8189)) {
+
+            while (true) {
+                try {
+                    new TCPConnection(this, serverSocket.accept());
+                } catch (IOException e) {
+                    System.out.println("TCPConnection exception " + e);
+                }
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public synchronized void onConnectionReady(TCPConnection tcpConnection) {
+        connections.add(tcpConnection);
+        sendToAllConnections("Client connected " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onReceiveString(TCPConnection tcpConnection, String value) {
+        sendToAllConnections(value);
+    }
+
+    @Override
+    public synchronized void onDisconnect(TCPConnection tcpConnection) {
+        connections.remove(tcpConnection);
+        sendToAllConnections("Client disconnected " + tcpConnection);
+    }
+
+    @Override
+    public synchronized void onException(TCPConnection tcpConnection, Exception e) {
+        System.out.println("TCPConnection exceptions " + e);
+    }
+
+    private void sendToAllConnections(String value) {
+        System.out.println(value);
+        final int size = connections.size();
+        for (int i = 0; i < size; i++) {
+            connections.get(i).sendString(value);
+        }
+    }
 }
